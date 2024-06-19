544c0aad1ba53fbb166e57efdaead1127fb2635e
Fathan Maulana <fthnmrh012@gmail.com>
2024-06-19 14:01:17 +0700

    refactor(auth) : removing firebase authentication
    remove firebase auth : logic, google-service.json and library dependencies
    
    if you want to reuse, you can get them in archive/firebase-auth branch

--- app/src/main/java/dev/capstone/satako_mobile/presentation/login/LoginFragment.kt
+++ app/src/main/java/dev/capstone/satako_mobile/presentation/login/LoginFragment.kt
@@ -7,27 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
-import androidx.credentials.CredentialManager
-import androidx.credentials.CustomCredential
-import androidx.credentials.GetCredentialRequest
-import androidx.credentials.GetCredentialResponse
-import androidx.credentials.exceptions.GetCredentialException
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import dev.capstone.satako_mobile.R
 import dev.capstone.satako_mobile.data.response.Result
 import dev.capstone.satako_mobile.databinding.FragmentLoginBinding
@@ -47,8 +32,6 @@ class LoginFragment : Fragment() {
     private val loginViewModel: LoginViewModel by viewModels {
         ViewModelFactory(requireContext())
     }
-
-    private lateinit var auth: FirebaseAuth
     private var loadingDialog: AlertDialog? = null
 
     override fun onCreateView(
@@ -62,9 +45,6 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        auth = Firebase.auth
-
         with(binding) {
             backButton.setOnClickListener {
                 view.findNavController().popBackStack()
@@ -76,13 +56,13 @@ class LoginFragment : Fragment() {
                 override fun onTextListener(s: CharSequence?) {
                     if (!s.isNullOrEmpty()) {
                         if (!s.toString().isEmailValid()) {
-                            binding.tvEmailError?.text = getString(R.string.invalid_email)
-                            binding.tvEmailError?.show()
+                            binding.tvEmailError.text = getString(R.string.invalid_email)
+                            binding.tvEmailError.show()
                         } else {
-                            binding.tvEmailError?.gone()
+                            binding.tvEmailError.gone()
                         }
                     } else {
-                        binding.tvEmailError?.gone()
+                        binding.tvEmailError.gone()
                     }
 
                 }
@@ -91,20 +71,17 @@ class LoginFragment : Fragment() {
                 override fun onTextListener(s: CharSequence?) {
                     if (!s.isNullOrEmpty()) {
                         if (s.toString().isEmpty()) {
-                            binding.tvPasswordError?.text = getString(R.string.password_empty)
-                            binding.tvPasswordError?.show()
+                            binding.tvPasswordError.text = getString(R.string.password_empty)
+                            binding.tvPasswordError.show()
                         } else {
-                            binding.tvEmailError?.gone()
+                            binding.tvEmailError.gone()
                         }
                     } else {
-                        binding.tvPasswordError?.gone()
+                        binding.tvPasswordError.gone()
                     }
                 }
             })
 
-//            googleSignInButton.setOnClickListener {
-//                signInWithGoogle()
-//            }
 
             signInButton.setOnClickListener {
                 val email = emailEditText.text.toString().trim()
@@ -147,83 +124,8 @@ class LoginFragment : Fragment() {
         }
     }
 
-    private fun signInWithGoogle() {
-        showLoading(true)
-        val credentialManager =
-            CredentialManager.create(requireContext())
-        val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(false)
-            .setServerClientId(getString(R.string.your_web_client_id))
-            .build()
-        val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()
-        lifecycleScope.launch {
-            try {
-                val result: GetCredentialResponse = credentialManager.getCredential(
-                    request = request,
-                    context = requireActivity(),
-                )
-                handleSignIn(result)
-            } catch (e: GetCredentialException) {
-                showLoading(false)
-                errorBottomSheet(getString(R.string.failed_to_get_credential))
-            }
-        }
-    }
 
-    private fun handleSignIn(result: GetCredentialResponse) {
-        showLoading(false)
-        when (val credential = result.credential) {
-            is CustomCredential -> {
-                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
-                    try {
-                        val googleIdTokenCredential =
-                            GoogleIdTokenCredential.createFrom(credential.data)
-                        firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
-                    } catch (e: GoogleIdTokenParsingException) {
-                        errorBottomSheet(getString(R.string.invalid_googleid))
-                    }
-                } else {
-                    errorBottomSheet(getString(R.string.unexpected_type_of_credential))
-                }
-            }
 
-            else -> {
-                errorBottomSheet(getString(R.string.unexpected_type_of_credential))
-            }
-        }
-    }
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential: AuthCredential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(requireActivity()) { task ->
-                if (task.isSuccessful) {
-                    val user: FirebaseUser? = auth.currentUser
-                    updateUI(user)
-                } else {
-                    errorBottomSheet(getString(R.string.failed_login))
-                    updateUI(null)
-                }
-            }
-    }
-
-    private fun updateUI(currentUser: FirebaseUser?) {
-        if (currentUser != null) {
-            val token = currentUser.getIdToken(true).toString()
-            val username = currentUser.displayName
-            val email = currentUser.email
-
-            if (username != null && email != null) {
-                loginViewModel.saveTokenSession(token, username, email) {
-                    toHome(requireView())
-                }
-            }
-        } else {
-            errorBottomSheet(getString(R.string.login_failed))
-        }
-    }
 
     private fun showLoading(isLoading: Boolean) {
         showLoadingDialog(isLoading)
@@ -256,20 +158,20 @@ class LoginFragment : Fragment() {
         password: String
     ): Boolean {
         if (email.isEmpty()) {
-            binding.tvEmailError?.text = getString(R.string.email_empty)
-            binding.tvEmailError?.show()
+            binding.tvEmailError.text = getString(R.string.email_empty)
+            binding.tvEmailError.show()
             return false
         }
 
         if (!email.isEmailValid()) {
-            binding.tvEmailError?.text = getString(R.string.invalid_email)
-            binding.tvEmailError?.show()
+            binding.tvEmailError.text = getString(R.string.invalid_email)
+            binding.tvEmailError.show()
             return false
         }
 
         if (password.isEmpty()) {
-            binding.tvPasswordError?.text = getString(R.string.password_empty)
-            binding.tvPasswordError?.show()
+            binding.tvPasswordError.text = getString(R.string.password_empty)
+            binding.tvPasswordError.show()
             return false
         }
         return true
